9dd6467f7a4de78f9be2709a13f941d09722f6e4
Matheus Leite <ma-matheus.2009@hotmail.com>
2025-03-21 23:05:48 -0300

    Fixing player movement and sync

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Player_Movement : NetworkBehaviour
 {
     [Header("Settings")]
-    [SerializeField] private SimpleKCC kcc;
+    //[SerializeField] private SimpleKCC kcc;
     [SerializeField] private float speed = 10f;
     [SerializeField] private float turnSpeed = 100f;
     [SerializeField] private float brakeForce = 50f;
@@ -16,8 +16,8 @@ public class Player_Movement : NetworkBehaviour
     [Networked] private NetworkButtons prevButtons { get; set; } = new NetworkButtons();
 
     private Rigidbody rb;
-    private float moveInput;
-    private float turnInput;
+    //private float moveInput;
+    //private float turnInput;
     private float forwardInput;
     
     void Awake() {
@@ -31,34 +31,43 @@ public class Player_Movement : NetworkBehaviour
     public override void Spawned() {
         base.Spawned();
 
-        kcc.SetGravity(Physics.gravity.y * 2f);
+        //kcc.SetGravity(Physics.gravity.y * 2f);
     }
 
     public override void FixedUpdateNetwork() {
         base.FixedUpdateNetwork();
 
         if (GetInput(out NetInput input)) {
-            Vector3 dir = kcc.TransformRotation * new Vector3(input.Direction.x, 0, input.Direction.y);
-            kcc.Move(dir.normalized * speed);
+            //Vector3 dir = kcc.TransformRotation * new Vector3(input.Direction.x, 0, input.Direction.y);
+            //kcc.Move(dir.normalized * speed);
+
+            if (rb.velocity.magnitude > 0.1f) transform.Rotate(0, input.Direction.x * turnSpeed * Time.deltaTime, 0);
+            rb.drag = input.Buttons.WasPressed(prevButtons, InputButton.Brake) ? brakeForce : 1;
+
+            if (input.Direction.y > 0) forwardInput = speed;
+            else if (input.Direction.y < 0) forwardInput = reverseSpeed;
+            else forwardInput = 0;
+
+            rb.velocity = transform.forward * forwardInput * input.Direction.y;
 
             prevButtons = input.Buttons;
         }
     }
 
     private void HandleInputMovement() {
-        moveInput = Input.GetAxis("Vertical");
+        /*moveInput = Input.GetAxis("Vertical");
         turnInput = Input.GetAxis("Horizontal");
 
         if (rb.velocity.magnitude > 0.1f) transform.Rotate(0, turnInput * turnSpeed * Time.deltaTime, 0);
-        rb.drag = Input.GetKey(KeyCode.Space) ? brakeForce : 1;
+        rb.drag = Input.GetKey(KeyCode.Space) ? brakeForce : 1;*/
     }
 
     private void HandlePhysics() {
-        if (moveInput > 0) forwardInput = speed;        
+        /*if (moveInput > 0) forwardInput = speed;        
         else if (moveInput < 0) forwardInput = reverseSpeed;
         else forwardInput = 0;
 
-        rb.velocity = transform.forward * forwardInput * moveInput;
+        rb.velocity = transform.forward * forwardInput * moveInput;*/
     }
 
     private void FixedUpdate() {
